7b1202e2a62e5d1cd2d95498b025881c3ce6c8a5
Miquel Perelló Nieto <perello.nieto@gmail.com>
2017-09-06 17:58:23 +0100

    Added Keras example

--- README.md
+++ README.md
@@ -26,3 +26,25 @@ Test scores per epoch during training
 [ 0.52  0.63  0.68  0.73  0.72  0.73  0.69  0.72  0.73  0.69  0.75  0.71
   0.75  0.76  0.71  0.69  0.76  0.69  0.71  0.69]
 ```
+
+## Example with Keras
+
+There is an additional example using Keras to specify Multilayer Perceptrons or
+Logistic regression. To run the example use these steps
+
+```bash
+git clone git@github.com:IRC-SPHERE/SklearnHyperStream.git
+cd SklearnHyperStream
+virtualenv venv
+. venv/bin/activate
+pip install -r requirements.txt
+pip install -r keras_requirements.txt
+python example_classifier.py --dataset digits --classifier mlp30ds40m --epochs 20 --seed 42
+```
+
+At the end of the training you should see the test scores during the training
+for each epoch
+
+```Python
+Test scores per epoch during training
+```
---
+++ example_classifier_keras.py
@@ -0,0 +1,178 @@
+import numpy as np
+import argparse
+
+from datetime import datetime, timedelta
+
+from sklearn import datasets, linear_model
+from sklearn.preprocessing import label_binarize
+
+from hyperstream import HyperStream, TimeInterval
+from hyperstream.utils import UTC
+
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.optimizers import SGD
+from keras.wrappers.scikit_learn import KerasClassifier
+
+class MyKerasClassifier(object):
+    """This is a modification of the KerasClassifier in order to keep the
+    labels with the original values.
+
+    Implementation of the scikit-learn classifier API for Keras.
+    """
+    #def __init__(self, optimizer='rmsprop', init='glorot_uniform', lr=1,
+    #             momentum=0.0, decay=0.0, nesterov=False , architecture='lr'):
+    #    return super(KerasClassifier, self).__init__()
+    #
+    def __init__(self, architecture='lr'):
+        self.model = None
+        self.architecture = architecture
+
+    def fit(self, x, y, **kwargs):
+        """Constructs a new model with `build_fn` & fit the model to `(x, y)`.
+        # Arguments
+            x : array-like, shape `(n_samples, n_features)`
+                Training samples where n_samples in the number of samples
+                and n_features is the number of features.
+            y : array-like, shape `(n_samples,)` or `(n_samples, n_outputs)`
+                True labels for X.
+            **kwargs: dictionary arguments
+                Legal arguments are the arguments of `Sequential.fit`
+        # Returns
+            history : object
+                details about the training history at each epoch.
+        """
+        if 'classes' in kwargs:
+            self.classes = kwargs['classes']
+
+        if y.shape[1] <= 1:
+            y = label_binarize(y, self.classes)
+        if self.model is None:
+            self.model = self.create_model(input_dim=x.shape[1],
+                                           output_size=y.shape[1],
+                                           architecture=self.architecture)
+        return self.model.fit(x, y, verbose=0)
+
+    def create_model(self, input_dim=1, output_size=1, optimizer='rmsprop',
+                     init='glorot_uniform', lr=1, momentum=0.0, decay=0.0,
+                     nesterov=False , architecture='lr'):
+        """
+        Parameters
+        ----------
+        architecture: string: lr, mlp100, mlp100d, mlp100d100d
+        """
+
+        model = Sequential()
+
+        previous_layer = input_dim
+        if architecture == 'lr':
+            model.add(Dense(output_size, input_shape=(input_dim,),
+                            kernel_initializer=init,
+                            activation='softmax'))
+        elif architecture.startswith('mlp'):
+            architecture = architecture[3:]
+            while len(architecture) > 0:
+                if architecture[0] == 'd':
+                    model.add(Dropout(0.5))
+                    architecture = architecture[1:]
+                elif architecture[0] == 's':
+                    model.add(Activation('sigmoid'))
+                    architecture = architecture[1:]
+                elif architecture[0] == 'm':
+                    model.add(Dense(output_size, kernel_initializer=init))
+                    model.add(Activation('softmax'))
+                    architecture = architecture[1:]
+                elif architecture[0].isdigit():
+                    i = 1
+                    while len(architecture) > i and architecture[i].isdigit():
+                        i += 1
+                    actual_layer = int(architecture[:i])
+                    model.add(Dense(actual_layer, input_dim=previous_layer,
+                                    kernel_initializer=init))
+                    architecture = architecture[i:]
+                    previous_layer = actual_layer
+                else:
+                    raise(ValueError, 'Architecture with a wrong specification')
+        else:
+            raise(ValueError, 'Architecture with a wrong specification')
+
+        if optimizer == 'sgd':
+            optimizer = SGD(lr=lr, momentum=momentum, decay=decay,
+                            nesterov=nesterov)
+
+        loss = 'categorical_crossentropy'
+        model.compile(loss=loss, optimizer=optimizer,
+                      metrics=['acc'])
+        return model
+
+    def predict(self, x):
+        return self.model.predict(x, verbose=0)
+
+    def score(self, x, y):
+        pred = self.model.predict_classes(x, verbose=0)
+        return np.mean(pred == y)
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('-c', '--classifier', type=str,
+                        default='lr',
+                        help='''Classifier to use. Working options: lr for a
+                        Logistic Regression. To specify Multilayer Perceptron
+                        use the following convention: mlp100dm for a Multilayer
+                        Perceptron with 100 hidden units, dropbout at 0.5 and
+                        SoftMax activation, mlp30ds40m for an MLP with 30
+                        hidden units with Dropout of 0.5, Sigmoid activation,
+                        40 hidden units and Softmax activation''')
+    parser.add_argument('-d', '--dataset', type=str, default='iris',
+                        help='''Dataset to use. Working options: iris,
+                                breast_cancer, wine, digits''')
+    parser.add_argument('-e', '--epochs', type=int, default=10,
+                        help='Number of epochs to run the classifier')
+    parser.add_argument('-s', '--seed', type=int, default=42,
+                        help='Seed for the data shuffle')
+
+    return parser.parse_args()
+
+
+def main(dataset, classifier, epochs, seed):
+    hs = HyperStream(loglevel=30)
+    print(hs)
+    print([p.channel_id_prefix for p in hs.config.plugins])
+
+    M = hs.channel_manager.memory
+
+    data = getattr(datasets, 'load_{}'.format(dataset))()
+    data_tool = hs.plugins.sklearn.tools.dataset(data, shuffle=True,
+                                                 epochs=epochs, seed=seed)
+    data_stream = M.get_or_create_stream('dataset')
+
+    model = MyKerasClassifier(architecture=classifier)
+    classifier_tool = hs.plugins.sklearn.tools.classifier(model)
+    classifier_stream = M.get_or_create_stream('classifier')
+
+    now = datetime.utcnow().replace(tzinfo=UTC)
+    now = (now - timedelta(hours=1)).replace(tzinfo=UTC)
+    before = datetime.utcfromtimestamp(0).replace(tzinfo=UTC)
+    ti = TimeInterval(before, now)
+
+    data_tool.execute(sources=[], sink=data_stream, interval=ti)
+
+    print("Example of a data stream")
+    key, value = data_stream.window().iteritems().next()
+    print('[%s]: %s' % (key, value))
+
+    classifier_tool.execute(sources=[data_stream], sink=classifier_stream,
+                            interval=ti)
+
+    scores = []
+    for key, value in classifier_stream.window():
+        scores.append(value['score'])
+
+    print("Test scores per epoch during training")
+    scores = np.array(scores).reshape(epochs, -1)
+    print(scores.mean(axis=1).round(decimals=2))
+
+
+if __name__ == '__main__':
+    arguments = get_arguments()
+    main(**vars(arguments))
